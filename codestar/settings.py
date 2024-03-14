470ad3b6f6d54a74e057a7bdf74fa61e4381fd51
Edward Bryan <eddy-bryan@hotmail.co.uk>
2024-03-14 19:20:05 +0000

    Amended views and templates to display approved comments.

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,10 +28,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-eddybryan-djangoblog-zsrnntzceik.ws-eu109.gitpod.io',
+    '8000-eddybryan-djangoblog-zsrnntzceik.ws-eu110.gitpod.io',
     '.herokuapp.com',
 ]
 
